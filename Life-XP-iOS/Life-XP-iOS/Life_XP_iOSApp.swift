ccad54687fa0aecd8b677ca79d430d6b67f710b8
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2026-01-10 15:08:35 -0500

    created XCode project

---
+++ Life-XP-iOS/Life-XP-iOS/Life_XP_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_XP_iOSApp.swift
+//  Life-XP-iOS
+//
+//  Created by Brandon Lamer-Connolly on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Life_XP_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Life-XP-iOS/Life-XP-iOSTests/Life_XP_iOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  Life_XP_iOSTests.swift
+//  Life-XP-iOSTests
+//
+//  Created by Brandon Lamer-Connolly on 1/10/26.
+//
+
+import Testing
+
+struct Life_XP_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
